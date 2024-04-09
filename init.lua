b4a2b3cff1676eb1ffeedf53a50a5b903550244c
Duco <drupsteen.duco@gmail.com>
2024-04-09 23:18:07 +0200

    Added treesitter textobjects

--- init.lua
+++ init.lua
@@ -694,21 +694,44 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<leader>i',
+          node_incremental = '<leader>i',
+          scope_incremental = '<leader>si',
+          node_decremental = '<leader>di',
+        },
+      },
+      textobjects = {
+        -- default mode V
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['fo'] = '@function.outer',
+            ['fi'] = '@function.inner',
+            ['co'] = '@class.outer',
+            ['ci'] = '@class.inner',
+          },
+        },
+      },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
