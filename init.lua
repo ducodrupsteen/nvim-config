b2ccb8e78a899ece66068475af1b29d2c666999e
Duco <drupsteen.duco@gmail.com>
2024-04-10 21:17:25 +0200

    changed some keybinds

--- init.lua
+++ init.lua
@@ -96,7 +96,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
@@ -725,10 +724,10 @@ require('lazy').setup({
           enable = true,
           lookahead = true,
           keymaps = {
-            ['fo'] = '@function.outer',
-            ['fi'] = '@function.inner',
-            ['co'] = '@class.outer',
-            ['ci'] = '@class.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
           },
         },
       },
