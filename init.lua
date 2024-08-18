d3a07e80ed77b6b4f54427193756b2ade9b6fe91
Duco <drupsteen.duco@gmail.com>
2024-08-18 15:34:51 +0200

    Revert netrw to normal list style

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ vim.g.nofsync = true
 vim.g.have_nerd_font = true
 
 vim.g.netrw_keepdir = 0
-vim.g.netrw_liststyle = 3
+-- vim.g.netrw_liststyle = 3
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
